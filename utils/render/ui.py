e76653b54061ffbb8bd2af4a559b5c84d8330b93
HoualefAkram <akram1232004@gmail.com>
2025-02-17 00:06:23 +0100

    updated html

--- utils/render/ui.py
+++ utils/render/ui.py
@@ -2,18 +2,41 @@ class Ui:
     @staticmethod
     def home():
         return """<!DOCTYPE html>
-<html lang="en">
-
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page avec Image, Texte et Lien</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            text-align: center;
+            margin: 50px;
+        }
+        img {
+            width: 300px;
+            height: auto;
+            border-radius: 10px;
+        }
+        a {
+            display: inline-block;
+            margin-top: 15px;
+            text-decoration: none;
+            color: white;
+            background-color: blue;
+            padding: 10px 15px;
+            border-radius: 5px;
+        }
+        a:hover {
+            background-color: darkblue;
+        }
+    </style>
+</head>
 <body>
-    <div>
-        <header>
-            <h1>M1 TLC<br>GROUP 2</h1>
-            Houalef Akram<br>
-            Hidjazi issam<br>
-            Fezzazi omar<br>
-            Mehdi larbi
-        </header>
-    </div>
+    <h1>Bienvenue sur ma page</h1>
+    <p>Teste page html cour ASR 2024-2025</p>
+    <img src="C:\Users\Larbi\Desktop\compus france\MEDHO.png" alt="Image Exemple">
+    <br>
+    <a href="https://www.univ-usto.dz/" target="_blank">Visitez le site</a>
 </body>
-
 </html>"""
