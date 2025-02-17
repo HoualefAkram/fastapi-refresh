6f0ae744ab7e85eb5f11731fde45ae4a6c76b74e
HoualefAkram <akram1232004@gmail.com>
2025-02-17 08:50:56 +0100

    added CORS

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from routers import math
 from utils.render.html import Html
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(math.router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/",response_class=HTMLResponse,status_code=200)
 def read_root():
