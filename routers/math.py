822a91885f947e35984914ae1847891bb210006a
HoualefAkram <akram1232004@gmail.com>
2025-02-23 20:21:46 +0100

    added logs

--- routers/math.py
+++ routers/math.py
@@ -1,26 +1,28 @@
 from fastapi import APIRouter
 from schemas import operation
 
-router = APIRouter(prefix="/math",tags=["math"])
+router = APIRouter(prefix="/math", tags=["math"])
 
 
 @router.post("/add", response_model=operation.OperationResponse)
 def add(numbers: operation.SumOperation):
+    print(f"{numbers.a} + {numbers.b} = {numbers.a + numbers.b}")
     return operation.OperationResponse(result=numbers.a + numbers.b)
 
 
 @router.post("/sub", response_model=operation.OperationResponse)
 def sub(numbers: operation.SubOperation):
-    return  operation.OperationResponse(result=numbers.a - numbers.b)
+    print(f"{numbers.a} - {numbers.b} = {numbers.a - numbers.b}")
+    return operation.OperationResponse(result=numbers.a - numbers.b)
+
 
 @router.post("/mul", response_model=operation.OperationResponse)
 def mul(numbers: operation.MulOperation):
-    return  operation.OperationResponse(result=numbers.a * numbers.b)
+    print(f"{numbers.a} * {numbers.b} = {numbers.a * numbers.b}")
+    return operation.OperationResponse(result=numbers.a * numbers.b)
+
 
 @router.post("/div", response_model=operation.OperationResponse)
 def div(numbers: operation.DivOperation):
+    print(f"{numbers.a} / {numbers.b} = {numbers.a / numbers.b}")
     return operation.OperationResponse(result=numbers.a / numbers.b)
-
-
-
-
